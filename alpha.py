7f9a5f9cc0e962c5ce24c8a08ec306c2f915d75f
bob dole <h.aijaz@gmail.com>
2013-04-10 22:16:36 -0500

    fixed path

--- alpha.py
+++ alpha.py
@@ -1,26 +1,28 @@
 import os
-from werkzeug.security import generate_password_hash
-from first import db, User
-if os.access('test.db', os.F_OK):
-	os.remove('test.db')
-	print 'deleted'
-	pass
+import sys
+print '%s' %sys.path[0]
+# from werkzeug.security import generate_password_hash
+# from first import db, User
+# if os.access('test.db', os.F_OK):
+	# os.remove('test.db')
+	# print 'deleted'
+	# pass
 
-db.create_all()
-test = generate_password_hash('test')
-new_user = User(fname = 'a', 
-				lname = 'b',
-				email = 'c',
-				username = 'a',
-				pword = test)
-db.session.add(new_user)
-db.session.commit()
-test = generate_password_hash('test')
-new_user = User(fname = 'b', 
-				lname = 'b',
-				email = 'ca',
-				username = 'b',
-				pword = test)
-db.session.add(new_user)
-db.session.commit()
-exit()
+# db.create_all()
+# test = generate_password_hash('test')
+# new_user = User(fname = 'a', 
+				# lname = 'b',
+				# email = 'c',
+				# username = 'a',
+				# pword = test)
+# db.session.add(new_user)
+# db.session.commit()
+# test = generate_password_hash('test')
+# new_user = User(fname = 'b', 
+				# lname = 'b',
+				# email = 'ca',
+				# username = 'b',
+				# pword = test)
+# db.session.add(new_user)
+# db.session.commit()
+# exit()
